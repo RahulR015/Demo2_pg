5ea5252cece9c68e48b15451efe726765112f76b
RahulR015 <rahulr15ec@gmail.com>
2025-09-27 01:35:36 -0700

    created add and sub

---
+++ Add.java
@@ -0,0 +1,10 @@
+class Add{
+    int a=10;
+    int b=20;
+    int c=30;
+    int d=40;
+
+    public static void main() {
+        System.out.println(a+b+c+d+e+f);
+    }
+}
---
+++ Sub.java
@@ -0,0 +1,10 @@
+class sub{
+    int a=10;
+    int b=20;
+    int c=30;
+    int d=40;
+    public staitic void main(String[] args)
+    {
+        System.out.println(a-b-c-d);
+    }
+}
